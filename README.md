86dd14bbd71965c5cc5accc4ccd9674aa11788ea
Kinky Cookie <kuki_kukula@yahoo.com>
2018-10-10 11:13:31 -0500

    Completed with Giphy API

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # GifTastic
+
+Game: Trivia Chicago \
+Created for: Northwestern Coding Bootcamp \
+Developer: Natalia Kukula \
+Deployment Date:  October 4, 2018 \
+Published: GitHub <https://nataliakukula.github.io/TriviaGame/> \
+Built with: HTML5, CSS3, Bootstrap, Javascript & jQuery
+
+## Summary: 
+
+![GifTastic](assets/images/gif-tastic.jpeg)
+
+* The user gets a response from the GIPHY API by clicking a button with the name of the musician and displaying 10 gifs to the screen.
+
+* The user gets to add their own button with their favorite musician.
+
+* The user can click a button to add a particular gif to a list of favorites.
+
+## Notes:
+
 First homework accessing API's
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,76 +1,20 @@
 // Create an array of strings called topics:
 var musicTopics = ["metallica", "britney spears", "aerosmith", "the beatles", "pink floyd", "the rollin stones", "justin timberlake", "nirvana", "ac/dc", "foo fighters", "radiohead", "pearl jam", "imagine dragons", "twenty one pilots", "coldplay", "drake", "ed sheeran", "bruno mars", "katy perry"];
-
+var gifCount = 0;
 // Take the topics in the array and create buttons in your HTML:
 function addButtons() {
 
     $(".buttons-div").empty();
     for (var i = 0; i < musicTopics.length; i++) {
         var button = $("<button>");
-        button.addClass("musician");
+        button.addClass("musician btn btn-outline-light");
         button.attr("data-name", musicTopics[i]);
         button.text(musicTopics[i]);
         $(".buttons-div").append(button);
     }
 }
-
 addButtons();
-
-//On a button click grab 10 static gifs from GIPHY API:
-$(".musician").on("click", function () {
-    var musicName = $(this).attr("data-name");
-
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        musicName + "&api_key=Zm8q9Qo27bvYE4mrLRmSA0verKDGSj0a&limit=10";
-
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    })
-        .then(function (response) {
-
-            console.log(response);
-            console.log(response.data[0].images.original_still.url);
-            console.log(response.data[0].images.original.url);
-            console.log(response.data[0].rating);
-
-            //Store the data from the AJAX request:
-            var resultsArray = response.data;
-            $(".gif-div").empty();
-            //Append the response to the screen:
-            for (var i = 0; i < resultsArray.length; i++) {
-
-                var musicDiv = $("<div>");
-                var rating = $("<p>").text("Rating: " + resultsArray[i].rating);
-                var musicImage = $("<img>");
-
-                musicImage.attr("src", resultsArray[i].images.original_still.url);
-                musicImage.attr("data-still", resultsArray[i].images.original_still.url);
-                musicImage.attr("data-animate", resultsArray[i].images.original.url);
-                musicImage.attr("data-state", "still");
-                musicImage.addClass("gif");
-
-                musicDiv.append(rating);
-                musicDiv.append(musicImage);
-
-                $(".gif-div").prepend(musicDiv);
-
-                //On the click event stop and start the animation of the gif:
-                $(".gif").on("click", function () {
-                    var state = $(this).attr("data-state");
-
-                    if (state === "still") {
-                        $(this).attr("src", $(this).attr("data-animate"));
-                        $(this).attr("data-state", "animate");
-                    } else {
-                        $(this).attr("src", $(this).attr("data-still"));
-                        $(this).attr("data-state", "still");
-                    };
-                });
-            };
-
-        });
-});
+makeAjaxCall();
 
 //Take the value from user input box and adds it into the topics array:
 $("#submit-button").on("click", function (event) {
@@ -79,4 +23,82 @@ $("#submit-button").on("click", function (event) {
     musicTopics.push(music);
     //Then call the function that takes each topic in the array remakes the buttons on the page:
     addButtons();
+    makeAjaxCall();
+});
+
+//On a button click grab 10 static gifs from GIPHY API:
+function makeAjaxCall() {
+    $(".musician").on("click", function () {
+        var musicName = $(this).attr("data-name");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+            musicName + "&api_key=Zm8q9Qo27bvYE4mrLRmSA0verKDGSj0a&limit=10";
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+            .then(function (response) {
+
+                console.log(response);
+
+                //Store the data from the AJAX request:
+                var resultsArray = response.data;
+                $(".gif-div").empty();
+                //Append the response to the screen:
+                for (var i = 0; i < resultsArray.length; i++) {
+
+                    gifCount++;
+
+                    var musicDiv = $("<figure>");
+                    musicDiv.addClass("figure");
+                    musicDiv.attr("id", "number-" + gifCount);
+
+
+                    var rating = $("<figcaption>").text("Rating: " + resultsArray[i].rating);
+                    rating.addClass("figure-caption text-right");
+
+                    var musicImage = $("<img>");
+                    musicImage.addClass("figure-img img-fluid rounded");
+                    musicImage.attr("src", resultsArray[i].images.fixed_width_still.url);
+                    musicImage.attr("data-still", resultsArray[i].images.fixed_width_still.url);
+                    musicImage.attr("data-animate", resultsArray[i].images.fixed_width.url);
+                    musicImage.attr("data-state", "still");
+                    musicImage.attr("id", "imgnumber-" + gifCount);
+                    musicImage.addClass("gif");
+
+                    console.log(musicImage.attr("id"), musicImage.attr("src"));
+                    console.log("Still img source: ", musicImage.attr("data-still"));
+                    console.log("Original img source: ", musicImage.attr("data-animate"));
+
+                    var favButton = $("<input>").val("Add to favorites!");
+                    favButton.addClass("btn btn-outline-danger fav-button");
+                    favButton.attr("data-button-number", gifCount);
+
+                    musicDiv.append(rating);
+                    musicDiv.append(musicImage);
+                    musicDiv.append(favButton);
+
+                    $(".gif-div").append(musicDiv);
+                };
+            });
+    });
+};
+
+//On the click event stop and start the animation of the gif:
+$(document).on("click", ".gif", function () {
+    var state = $(this).attr("data-state");
+    console.log("State: ", state);
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+    } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+    };
+});
+//On "click" event add the gif to favorites and remove it from the list:
+$(document).on("click", ".fav-button", function () {
+    var buttonNumber = $(this).attr("data-button-number");
+    $("#favorites").prepend($("#imgnumber-" + buttonNumber));
+    $("#number-" + buttonNumber).remove();
 });
